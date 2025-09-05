7a45d20a123ab66ad3d2775bd341faac08e8b40f
Théo Lanord <58632394+enwiro@users.noreply.github.com>
2025-09-05 10:32:12 +0200

    Create index.js for java

---
+++ training-tdd/index.js
@@ -0,0 +1,17 @@
+{
+  "title": "Java Hello World",
+  "description": "Compiler et exécuter un programme Java sur Killercoda",
+  "details": {
+    "intro": "intro.md",
+    "steps": [
+      { "title": "Installer Java et exécuter Hello.java", "text": "1_installation.md" }
+    ],
+    "finish": "finish.md"
+  },
+  "backend": {
+    "imageid": "ubuntu"
+  },
+  "environment": {
+    "uilayout": "ide"
+  }
+}
